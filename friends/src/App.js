79ba4f9b65cbd70c8cd08ef523fb00de2cd8a82f
Josh Akeman <joshakeman@gmail.com>
2019-04-11 16:59:21 -0400

    login working

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css';
+
+import Login from './components/Login'
+import Home from './components/Home'
+import FriendsList from './components/FriendsList'
+import PrivateRoute from './components/PrivateRoute'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Friends app</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="nav-bar">
+            <div className="nav-link">
+              <Link to="/login">Login</Link>
+            </div>
+            <div className="nav-link">
+            </div>
+          </div>
+
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <PrivateRoute exact path="/protected" component={FriendsList} />
+        </div>
+      </Router>  
     );
   }
 }
--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,9 +1,30 @@
+import {
+    LOGIN_START,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE
+} from '../actions'
+
 const initialState = {
-    friends: []
+    friends: [],
+    error: '',
+    fetchingData: false,
+    loggingIn: false
 }
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
+        case LOGIN_START:
+            return {
+                ...state,
+                error: '',
+                loggingIn: true
+            }
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
+                error: '',
+                loggingIn: false
+            }
         default:
         return state
 
